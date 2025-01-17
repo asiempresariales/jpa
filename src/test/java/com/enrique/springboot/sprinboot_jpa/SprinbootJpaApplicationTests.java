60022d140a9f5fe60fe2b0944a5977131e43a619
ascencio <2020371104@uteq.edu.mx>
2025-01-16 21:19:49 -0600

    inicial

---
+++ src/test/java/com/enrique/springboot/sprinboot_jpa/SprinbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enrique.springboot.sprinboot_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
